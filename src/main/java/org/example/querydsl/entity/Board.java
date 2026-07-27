2af0955e502c4bd18bfafac64fbf5de3cd5b58ef
a1l1ke <onetenthplease@gmail.com>
2026-07-27 14:45:23 +0900

    Querydsl 설정 및 Board 엔티티 추가

---
+++ src/main/java/org/example/querydsl/entity/Board.java
@@ -0,0 +1,14 @@
+package org.example.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+}
